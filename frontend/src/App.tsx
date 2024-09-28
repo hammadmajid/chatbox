218355c8229eda5b9e772f79f5fdddc73e4a7525
Hammad Majid <hammadmajid@proton.me>
2024-09-29 02:11:55 +0500

    remove generated code & use shadcn Button component for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='space-y-6'>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Count is {count}
+        </h1>
+        <div className='flex gap-2 justify-center'>
+          <Button variant="secondary" onClick={() => setCount((count) => count - 1)}>
+            Decrement
+          </Button >
+          <Button onClick={() => setCount((count) => count + 1)}>
+            Increment
+          </Button >
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
